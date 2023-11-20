eead3ffb75d2381e3f53e8eed6b4ccd2b03edc08
Tomer Salakoff <tomer@om2.com>
2023-11-20 10:25:06 +0200

    Refactor requested_vpc_peering_connection_id in main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,7 @@ locals {
   accepter_enabled  = var.create && var.accepter_enabled
   accepter_count    = var.create && var.accepter_enabled ? 1 : 0
 
-  requested_vpc_peering_connection_id = (
-    local.accepter_enabled == true
-    ? try(
-      var.peering_connection_id_to_accept,
-      aws_vpc_peering_connection.requester[*].id
-    )
-    : null
-  )
+  requested_vpc_peering_connection_id = try(aws_vpc_peering_connection.requester[*].id, var.peering_connection_id_to_accept)
 
   active_vpc_peering_connection_id = local.accepter_enabled ? join("", aws_vpc_peering_connection_accepter.accepter[*].id) : null
 }
