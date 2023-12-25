1cce29241afd7c57d43677cb7f32e635d2458d2c
Tomer Salakoff <tomer@om2.com>
2023-12-25 13:56:03 +0200

    Amened previous commit

--- accepter.tf
+++ accepter.tf
@@ -93,7 +93,7 @@ resource "aws_route" "accepter" {
   count                     = local.accepter_enabled ? local.accepter_aws_route_table_ids_count * local.requester_cidr_block_associations_count : 0
   route_table_id            = local.accepter_aws_route_table_ids[floor(count.index / local.requester_cidr_block_associations_count)]
   destination_cidr_block    = local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"]
-  vpc_peering_connection_id = local.active_vpc_peering_connection_id
+  vpc_peering_connection_id = local.requested_vpc_peering_connection_id
   depends_on = [
     data.aws_route_tables.accepter,
     aws_vpc_peering_connection_accepter.accepter,
